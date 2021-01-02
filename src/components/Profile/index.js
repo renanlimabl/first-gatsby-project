f99cdf179d5b05664b5b1bd6b6930a369263eed9
renanlimabl <renanlimabl@gmail.com>
2021-01-02 06:30:12 -0300

    Adiciona profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div class="profile-wrapper">
+    <h1>Matt Foo</h1>
+    <h2>Programador Backend</h2>
+    <p>Lorem ipsum, bla bla bla Lorem ipsum, bla bla bla Lorem ipsum, bla bla bla.</p>
+  </div>
+)
+
+export default Profile;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Profile from './Profile';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
+      <aside>
+        <Profile />
+      </aside>
       <main>{children}</main>
     </>
   )
