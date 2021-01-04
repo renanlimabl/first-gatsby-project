5f8b103f2afebc587c52cf28cd971d0f34d0de10
renanlimabl <renanlimabl@gmail.com>
2021-01-04 03:09:02 -0300

    Adiciona o uso do useStaticQuery do graphql

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Profile = () => (
-  <>
-    <StaticQuery
-      query={graphql`
-      query MySitemetadata {
-        site {
-          siteMetadata {
-            title
-            position
-            description
-          }
+const Profile = () => {
+  const { site: { siteMetadata: { title, position, description } }
+  } = useStaticQuery(graphql`
+    query MySitemetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
         }
       }
-    `}
-      render={({ site: { siteMetadata: { title, position, description } } }) => (
-        <div class="profile-wrapper">
-          <h1>{title}</h1>
-          <h2>{position}</h2>
-          <p>{description}</p>
-        </div>
-      )}
-    />
-  </>
-)
+    }
+  `)
+
+
+  return (
+    <div class="profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+
 
 export default Profile;
