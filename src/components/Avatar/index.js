05cd5a5ff49f63ae2adb22f18ebf26bc0274de45
renanlimabl <renanlimabl@gmail.com>
2021-01-05 14:03:32 -0300

    Adiciona diferença entre imagem fixed e fluid

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
+// utilizar fluid para imagens responsivas.
 const Avatar = () => {
   const { avatarImage } = useStaticQuery(
     graphql`
