2cfd18d0307a163731c4811fc840fe98bf7466b1
renanlimabl <renanlimabl@gmail.com>
2021-01-06 16:29:59 -0300

    Adiciona styled-components e plugin para gatsby/styled-components

---
+++ src/components/Layout/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Profile from '../Profile';
+import GlobalStyle from '../../styles/global';
+import styled from 'styled-components';
+
+const LayoutWrapper = styled.section`
+  display: flex;
+`;
+
+const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
+
+const Layout = ({ children }) => {
+  return (
+    <LayoutWrapper>
+      <GlobalStyle />
+      <aside>
+        <Profile />
+      </aside>
+      <LayoutMain>
+        {children}
+      </LayoutMain>
+    </LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
